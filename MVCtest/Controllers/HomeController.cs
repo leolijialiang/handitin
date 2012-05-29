1fe15abbf4a72ccf79db15c43b17da0b78373c4f
lijinglue <hkallenli@gmail.com>
2012-05-29 19:33:13 +0800

    MVC3 .net, db design finished 	new file:   MVCtest.suo

---
+++ MVCtest/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCtest.Models;
+
+namespace MVCtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour < 12 ? "good morning" : "good afternood";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid){
+                return View("Thanks", guestResponse);
+            }else{
+                return View();
+            }
+                       
+        }
+
+
+    }
+}
